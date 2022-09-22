c75393ed9470f712caa12feb4c8d6294f44e6ab8
Reinaldo Junior <rsljunior@Live.com>
2022-09-22 20:01:54 -0300

    operadores aritmeticos

---
+++ 06-operadores-arit.js
@@ -0,0 +1,57 @@
+/**
+ * Aritméticos
+ * + -> Adição / Concatenação (se for String)
+ * - -> Subtração
+ *  / -> Divisão
+ *  * -> Multiplicação
+ * ** -> potenciação
+ * % Resto da divisão
+ *
+ * Precedencia () ** * / % + -
+ * Incremento = ++
+ * Decremento = --
+ * Operadores de atribuição
+ * parseInt(inteiro), parseFloat(decimais)
+ */
+
+/*const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+
+console.log(num1 + num2 * num3);
+
+let contador = 10;
+contador++; //2
+contador++; //3
+contador++; //4
+contador++; //5/
+
+
+//console.log(--contador);
+//console.log(contador);
+
+const passo = 2;
+let contador = 0;
+contador = contador + passo;
+console.log(contador);
+
+contador = contador + passo;
+console.log(contador);
+
+contador = contador + passo;
+console.log(contador);
+
+
+//const passo = 50;
+let contador = 2;
+contador **= 10; //contador = contador + 2
+console.log(contador);
+
+*/
+
+const num1 = 10;
+const num2 = Number('5.2');
+
+//NaN Not a Number
+console.log(num1 + num2);
+console.log(typeof num2);
