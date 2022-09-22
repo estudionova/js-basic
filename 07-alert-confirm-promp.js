aa9afa9c551fa53fea9bb6caa5dcac05e33c2e32
Reinaldo Junior <rsljunior@Live.com>
2022-09-22 20:43:15 -0300

    alert-confirm-prompt

---
+++ 07-alert-confirm-promp.js
@@ -0,0 +1,32 @@
+/*window.alert('Mensagem');
+window.confirm('Deseja realmente apagar?');
+window.prompt('Digite o seu nome:');
+
+
+
+alert('Olá Mundo!');
+console.log('olá');
+confirm('Confirma que deseja apagar?');
+
+const confirma = confirm('Realmente deseja apagar?');
+
+
+
+let confirma = confirm('Qualquer mensagem');
+let num1 = prompt('Digite um numero:');
+let num2 = prompt('Digite outro numero:');
+*/
+
+
+let num1 = prompt ('Digite um número:');
+let num2 = prompt ('Digite outro número:');
+
+
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+const soma = num1 + num2;
+
+console.log(soma);
+alert(`O resultado da soma é: ${soma} `);
