b15785c194a484ed8a791d172bbca7ccd0cbbcdc
Reinaldo Junior <rsljunior@Live.com>
2022-09-27 19:50:05 -0300

    numbers

---
+++ 11-numbers.js
@@ -0,0 +1,37 @@
+//IEEE 754-2008
+
+let num1 = 0.7; //number
+let num2 = 0.1; //number
+
+/*
+num1 += num2; // num1 = num1 + num2 => 0.8
+num1 += num2; // num1 = num1 + num2 => 0.9
+num1 += num2; // num1 = num1 + num2 => 1.0
+
+
+//num1 = parseFloat(num1.toFixed(2)); //parseFloat ou Number
+
+num1 = ((num1 * 100) + (num2 * 100)) / 100; //0.8
+num1 = ((num1 * 100) + (num2 * 100)) / 100; //0.9
+num1 = ((num1 * 100) + (num2 * 100)) / 100; //1.0
+*/
+
+
+console.log(num1);
+console.log(Number.isInteger(num1));
+
+
+/*
+console.log(num1.toString() + num2); //converte temporario
+num1 = num1.toString();
+console.log(typeof num1);
+console.log(num1.toString(2));
+console.log(num1.toFixed(2)); // arrendodado, não altera o valor real
+console.log(Number.isInteger(num1)); //retorna se o numero é inteiro v/f
+
+let temp = num1 + '5';
+
+console.log(Number.isNaN(temp)); //NaN
+console.log(temp);
+*/
+
