e891c9450c64499341d41899e284c0b4940fae40
Reinaldo Junior <rsljunior@Live.com>
2022-09-29 09:03:05 -0300

    Math

---
+++ 12-math.js
@@ -0,0 +1,21 @@
+
+let num1 = 25;
+
+//let num2 = Math.floor(num1);    //arrendondar para baixo
+//let num2 = Math.ceil(num1);    //arrendondar para cima
+//let num2 = Math.ceil(num1);    //arrendondar para cima
+//let num2 = Math.round(num1);  // arrendonda pro mais proximo ( 9.50 arredonda pra cima)
+//let num2 = Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6);    // pega o maior numero
+//let num2 = Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6);    // pega o menor numero
+//console.log(num2);
+
+//const aleatorio = Math.round( Math.random() * (10 -5) + 5); // numero aleatorio
+//console.log(aleatorio);
+
+//console.log(Math.PI);// PI
+//console.log(Math.pow(2,10)); //Pontenciação
+
+//console.log(num1 ** (1/2)); //raiz quadrada
+//console.log(num1 ** 0.5); //raiz quadrada
+
+console.log(100 / 0.00000000000000); // javascript permite divisão por 0, e em boolean avalia com TRUE (verdadeiro)
