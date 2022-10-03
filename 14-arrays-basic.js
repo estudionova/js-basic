d300ab7e4da32ea5088a4ab8e05ec3f770221787
Reinaldo Junior <rsljunior@Live.com>
2022-10-03 20:03:32 -0300

    arrays basic

---
+++ 14-arrays-basic.js
@@ -0,0 +1,59 @@
+//            01234567....... 
+//const nome = 'Reinaldo Junior';
+//console.log(nome[10]);
+
+//                  0          1        2
+const alunos = ['Reinaldo', 'Maria', 'João']; //qual tipo de dado
+
+console.log(typeof alunos); //retorna objeto
+console.log(alunos instanceof Array);
+
+
+
+//console.log(alunos.length);
+
+
+
+
+/*
+
+alunos.push('Luiza');
+alunos.push('Eduardo');
+
+console.log(alunos.slice(0, -3)); //positivo ou negativo
+
+
+console.log(alunos[50]); //undefined posição no vetor não existe
+
+delete alunos[1];
+
+alunos.shift(); //remove com começo
+
+
+const removido = alunos.pop(); //remove elemento do final (retira o ultimo)
+console.log(removido);
+
+alunos.unshift('Luiza'); //Adiciona no indice 0 (Começo)
+alunos.unshift('Fábio'); //Adiciona no indice 0 (Começo)
+
+
+alunos[alunos.length] = 'Luiza'; //Adiciona no fim
+alunos[alunos.length] = 'Fábio';
+alunos[alunos.length] = 'Luana';
+
+
+alunos.push('Otávio');
+alunos.push('Luiza');
+alunos.push('Fábio');
+*/
+
+
+/*
+alunos[0] = 'Eduardo'; 
+alunos[3] = 'Luiza';
+
+
+console.log(alunos);
+console.log(alunos[0]); //Altera
+console.log(alunos[2]); //Adiciona
+*/
