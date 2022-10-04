a4bd92b748492240bf3735be8ac652642181d3fc
Reinaldo Junior <rsljunior@Live.com>
2022-10-04 11:50:17 -0300

    funcoes-basic

---
+++ 15-funcoes-basic.js
@@ -0,0 +1,53 @@
+
+const raiz = (n) => n ** 0.5;
+;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+
+
+/*
+const raiz = function (n){
+  return n ** 0.5;
+
+};
+console.log(raiz(25));
+
+
+
+function soma (x, y){
+  const resultado = x + y;
+  return resultado; //função encerra aqui, não executa nada abaixo
+
+}
+
+const resultado = soma(2, 2);
+console.log(resultado);
+
+//console.log(soma(2, 2));
+//console.log(resultado); 
+
+
+console.log(soma(2 , 2));
+console.log(soma(2 , 4));
+console.log(soma(2 , 9));
+*/
+
+
+/*function saudacao(nome){
+   
+    return `Bom dia ${nome}`;
+
+}
+
+const variavel = saudacao('Reinaldo');
+console.log(variavel); //undefined
+
+
+
+saudacao('Luiz');
+saudacao('Maria');
+saudacao('Felipe');
+*/
